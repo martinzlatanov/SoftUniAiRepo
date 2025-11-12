8b41a7b6ea367d8b0acb35428363301d76531b1a
Martin Zlatanov <mzlatanov@Martins-MBP.localdomain>
2025-11-12 07:57:24 +0200

    feat: add functions.js

---
+++ Strings.js
@@ -0,0 +1,8 @@
+let firstName = "Martin";
+let lastName = "Zlatanov";    
+let age = 45;
+let str = firstName + " " + lastName + " @ " + age + " years old.";
+console.log(str);
+
+let text = `${firstName} ${lastName} @ ${age} years old.`;
+console.log(text);
---
+++ functions.js
@@ -0,0 +1,8 @@
+function printHello() {
+    console.log("Hello, World!");
+    console.log("This is a simple function.");
+}
+
+for (let i = 0; i < 3; i++) {
+    printHello();
+}
